bf83b83923f494dd3b45a6e7a2c65a5929ea786e
kevincvlam <i.am.kev@gmail.com>
2014-02-26 23:25:23 -0500

    buildings

--- app/views/buildings.blade.php
+++ app/views/buildings.blade.php
@@ -5,5 +5,11 @@
     <h2>Buildings Table</h2>
     @foreach($buildings as $building)
         <p>{{ $building->apn }}</p>
+        @foreach($population in $populations)
+            @if( $building->apn == $population->apn )
+                --- <i>{{ $population-->activeconn }}</i>
+                --- {{ $popluation-->timestamp }}<br>
+        @endforeach
+
     @endforeach
 @stop
