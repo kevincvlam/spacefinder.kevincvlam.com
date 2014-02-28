b97543cb561573277350b9095ff192fee09ddd24
kevincvlam <i.am.kev@gmail.com>
2014-02-28 14:08:08 -0500

    graphdatatest

--- app/views/graphdatatest.blade.php
+++ app/views/graphdatatest.blade.php
@@ -7,11 +7,14 @@ include '/home7/kevincvl/public_html/spacefinder/library/spaceFunctions.php';
     $con = connectToDB();
 
     $result = getTimeSeries(12, 'Robarts Library', '1', 0, $con);
-    while($row = $result->fetch_row()){
-        echo "['Active Connections, 'Time'],<br>";
+    echo "['Active Connections, 'Time'],<br>";
+    echo $results[lengths][num_rows];
+    /*
+    for($i = 0; $i < $result[lengths][num_rows]; $i++){
+        $row = $result->fetch_row();
         echo "[$row[0], '$row[1]'],<br>";
     }
-
+*/
 
 ?>
 @stop
