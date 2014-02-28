ba432dbb8746242b2ff061e3e65ef4ca02dbe61e
kevincvlam <i.am.kev@gmail.com>
2014-02-28 14:36:56 -0500

    charttest

--- library/spaceFunctions.php
+++ library/spaceFunctions.php
@@ -232,7 +232,7 @@ function getTimeSeries($numEntries, $building, $floor, $area, $connect){
 	$query = $query . "')";													//building info subquery
 	$query = $query . ") as relevantVals
      GROUP BY timestamp
-     ORDER BY timestamp desc
+     ORDER BY timestamp asc
      LIMIT " . $numEntries;				
 	
 	//echo $query;   //display query for testing
