9b17dea0b632a4c36111024beabbc80ede63da0b
kevincvlam <i.am.kev@gmail.com>
2014-02-28 10:41:47 -0500

    line chart

--- app/views/charttest.blade.php
+++ app/views/charttest.blade.php
@@ -1,30 +1,47 @@
-<html>
+<html xmlns="http://www.w3.org/1999/xhtml">
   <head>
-    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+    <title>
+      Google Visualization API Sample
+    </title>
+    <script type="text/javascript" src="//www.google.com/jsapi"></script>
     <script type="text/javascript">
-      google.load("visualization", "1", {packages:["corechart"]});
-      google.setOnLoadCallback(drawChart);
-      function drawChart() {
+      google.load('visualization', '1', {packages: ['corechart']});
+    </script>
+    <script type="text/javascript">
+      function drawVisualization() {
+        // Create and populate the data table.
         var data = google.visualization.arrayToDataTable([
-          ['Year', 'Sales', 'Expenses'],
-          ['2004',  1000,      400],
-          ['2005',  1170,      460],
-          ['2006',  660,       1120],
-          ['2007',  1030,      540]
+          ['x', 'Cats', 'Blanket 1', 'Blanket 2'],
+          ['A',   1,       1,           0.5],
+          ['B',   2,       0.5,         1],
+          ['C',   4,       1,           0.5],
+          ['D',   8,       0.5,         1],
+          ['E',   7,       1,           0.5],
+          ['F',   7,       0.5,         1],
+          ['G',   8,       1,           0.5],
+          ['H',   4,       0.5,         1],
+          ['I',   2,       1,           0.5],
+          ['J',   3.5,     0.5,         1],
+          ['K',   3,       1,           0.5],
+          ['L',   3.5,     0.5,         1],
+          ['M',   1,       1,           0.5],
+          ['N',   1,       0.5,         1]
         ]);
-
-        var options = {
-          title: 'Company Performance'
-          curveType: 'function',
-          legend: { position: 'bottom' }
-        };
-
-        var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-        chart.draw(data, options);
+      
+        // Create and draw the visualization.
+        new google.visualization.LineChart(document.getElementById('visualization')).
+            draw(data, {curveType: "function",
+                        width: 500, height: 400,
+                        vAxis: {maxValue: 10}}
+                );
       }
+      
+
+      google.setOnLoadCallback(drawVisualization);
     </script>
   </head>
-  <body>
-    <div id="chart_div" style="width: 900px; height: 500px;"></div>
+  <body style="font-family: Arial;border: 0 none;">
+    <div id="visualization" style="width: 500px; height: 400px;"></div>
   </body>
 </html>
