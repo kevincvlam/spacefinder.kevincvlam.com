4c217354b852f4068ba61ac66fb4d0907f53cb35
kevincvlam <i.am.kev@gmail.com>
2014-02-28 12:05:42 -0500

    fixing query

--- library/spaceFunctions.php
+++ library/spaceFunctions.php
@@ -63,7 +63,7 @@ function getPopulation($building, $floor, $area, $connect){
 	}
 	//construct query:
 	$query  = "SELECT SUM(activeconn) FROM	";						//summing query	
-	$query = $query . "SELECT apn, activeconn, timestamp 
+	$query = $query . "(SELECT apn, activeconn, timestamp 
     FROM
     (SELECT apn, activeconn, timestamp FROM populations		
 	WHERE apn IN (SELECT apn FROM buildings";  								//building info subquery
