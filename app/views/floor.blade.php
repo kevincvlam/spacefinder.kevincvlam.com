e6975dd75687891dbf1919984cc360cdc7c41b7d
kevincvlam <i.am.kev@gmail.com>
2014-02-28 12:51:26 -0700

    floor

--- app/views/floor.blade.php
+++ app/views/floor.blade.php
@@ -14,7 +14,7 @@
         <?php
         include '/home7/kevincvl/public_html/spacefinder/library/spaceFunctions.php';
         $con = connectToDB();
-        printGoogleChartData(72, 'Robarts Library', '{{floornum}}', 0, $con); 
+        printGoogleChartData(72, 'Robarts Library', '{{{floornum}}}', 0, $con); 
         ?>
         // Create and draw the visualization.
         new google.visualization.SteppedAreaChart(document.getElementById('visualization')).
@@ -27,7 +27,8 @@
       google.setOnLoadCallback(drawVisualization);
     </script>
   </head>
-  <body style="font-family: Arial;border: 0 none;">
+  <body style="font-family: Arial;border: 0 none;"> 
+	Floor : {{{floornum}}}<br>
     <div id="visualization" style="width: 1000px; height: 400px;"></div>
   </body>
 </html>
