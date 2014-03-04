0a36c329f269e4cd2b162feeafc150db9da4c1fd
kevincvlam <i.am.kev@gmail.com>
2014-03-04 14:52:35 -0700

    changing thresholds and maxpop

--- library/findBusyness.php
+++ library/findBusyness.php
@@ -1,9 +1,9 @@
 <?php
 // Define a few constants that are reused
 define('EMPTY_THRESHOLD', '15');
-define('HASSPACE_THRESHOLD', '40');
+define('HASSPACE_THRESHOLD', '50');
 define('BUSY_THRESHOLD', '70');
-define('CROWDED_THRESHOLD', '90');
+define('CROWDED_THRESHOLD', '100');
 
 
 //This function will be called by external html ie front end UI
