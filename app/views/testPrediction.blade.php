cd7f694a35cfcbf66eb9aa26a3ccf8dc8f22fcff
kevincvlam <i.am.kev@gmail.com>
2014-03-25 14:26:25 -0400

    testPrediction

---
+++ app/views/testPrediction.blade.php
@@ -0,0 +1,41 @@
+<h1> Robarts Library Prediction </h1>
+<br>
+<br>
+<h2> Robarts Library Floor 1 : </h2>
+<?php
+include 'prediction.php';
+getFloorPrediction('Robarts Library', 1, 0);	  
+?>
+<h2> Robarts Library Floor 2 : </h2>
+<?php
+getFloorPrediction('Robarts Library', 2, 0);	  
+?>
+<h2> Robarts Library Floor 3 : </h2>
+<?php
+getFloorPrediction('Robarts Library', 3, 0);	  
+?>
+<h2> Robarts Library Floor 4 : </h2>
+<?php
+getFloorPrediction('Robarts Library', 4, 0);	  
+?>
+<h2> Robarts Library Floor 9 : </h2>
+<?php
+getFloorPrediction('Robarts Library', 9, 0);	  
+?>
+<h2> Robarts Library Floor 10 : </h2>
+<?php
+getFloorPrediction('Robarts Library', 10, 0);	  
+?>
+<h2> Robarts Library Floor 11 : </h2>
+<?php
+getFloorPrediction('Robarts Library', 11, 0);	  
+?>
+<h2> Robarts Library Floor 12 : </h2>
+<?php
+getFloorPrediction('Robarts Library', 12, 0);	  
+?>
+<h2> Robarts Library Floor 13 : </h2>
+<?php
+getFloorPrediction('Robarts Library', 13, 0);	  
+?>
+
