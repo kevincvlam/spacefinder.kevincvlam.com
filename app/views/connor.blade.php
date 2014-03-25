72d011429bfbe299bbe4d58da8b1b962ec4b5b22
holmesco <connor.holmes@mail.utoronto.ca>
2014-03-25 00:21:08 -0400

    testing busynessIndex

--- app/views/connor.blade.php
+++ app/views/connor.blade.php
@@ -5,8 +5,8 @@
 <?php
 	include '/home7/kevincvl/public_html/spacefinder/library/spaceFunctions.php';
 
-	echo "Robarts Library: " . businessIndex('Robarts Library') . "<br>";
-	echo "1st floor: " . businessIndex('Robarts Library', 1);
+	echo "Robarts Library: " . busynessIndex('Robarts Library') . "<br>";
+	echo "1st floor: " . busynessIndex('Robarts Library', 1);
 	
 
 ?>
