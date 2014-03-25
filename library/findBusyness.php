7ae331d16baccba26475db6c42d68cb18842a147
kevincvlam <i.am.kev@gmail.com>
2014-03-25 17:21:51 -0400

    consolidating php

--- library/findBusyness.php
+++ library/findBusyness.php
@@ -149,4 +149,54 @@ function findBusynessIndex ($building, $floor, $area){
     return $busyness;
 }
 
+function getFloorPrediction($building, $floor, $timeRange){
+   	if (!$building) echo "Error: No Building Indicated <br>";
+	
+	//connect to the SpaceFinder database
+	$connect = connectToDB();
+	
+	if($connect->errno != 0){
+		echo "Error with connection passed to function getPopulation";
+		exit();
+	}
+	if (!$timeRange) $timeRange = 3;
+	$endTime = 172 - $timeRange;
+	/*$query = "(SELECT SUM(activeconn) FROM populations WHERE timestamp = (SELECT MIN(timestamp) FROM populations 
+	         WHERE timestamp >= NOW() - INTERVAL 172 HOUR AND timestamp <= NOW() - INTERVAL ". $endTime ." HOUR ) 
+			 AND apn IN (SELECT apn FROM buildings";
+	if($building) $query = $query . " WHERE bname = '" . $building;			//remember single quotes for sql query
+	if($floor) $query = $query . "' AND bfloor = '" . $floor;
+	$query = $query . "'))";
+	//echo $query." <br>";
+	$newQuery = str_replace('MIN', 'MAX', $query);
+	$query = $query . " UNION " . $newQuery;
+	echo $query." <br>";*/
+	
+	$query = "(SELECT SUM(activeconn) FROM populations WHERE timestamp IN (SELECT * FROM (((SELECT MIN(timestamp) FROM populations WHERE timestamp >= NOW() - INTERVAL 190 HOUR AND timestamp <= NOW() - INTERVAL 169 HOUR ))
+			UNION
+			((SELECT MAX(timestamp)  FROM populations WHERE timestamp >= NOW() - INTERVAL 190 HOUR AND timestamp <= NOW() - INTERVAL ". $endTime ." HOUR ))) AS timestamp)
+			 AND apn IN (SELECT apn FROM buildings";
+	if($building) $query = $query . " WHERE bname = '" . $building;			//remember single quotes for sql query
+	if($floor) $query = $query . "' AND bfloor = '" . $floor;
+	$query = $query . "') GROUP BY timestamp)";
+	//echo $query." <br>";
+
+		
+	//call query
+	if($result = $connect->query($query)){					//if successful result
+		$row = $result->fetch_array();							//return result
+		$startPop =(int) $row[0];
+		$row = $result->fetch_array();	
+		$endPop = (int) $row[0];
+		$result->close();
+	}
+	$slope = 0;
+	$message = "";
+	if ($endPop) { 
+		$slope = ($endPop - $startPop)/ $startPop;	
+	} else {
+		echo "Error: End Population is zero";
+		return 0;
+	}
+	
 ?>
