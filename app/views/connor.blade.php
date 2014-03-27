abb7d6ef3ff01e5bd08ba8221d5ae1f53470ebcd
holmesco <connor.holmes@mail.utoronto.ca>
2014-03-27 15:55:28 -0400

    test maps

--- app/views/connor.blade.php
+++ app/views/connor.blade.php
@@ -7,9 +7,9 @@
 
 	echo "Robarts Library: " . busynessIndex('Robarts Library') . "<br>";
 	echo "1st floor: " . busynessIndex('Robarts Library', 1) . "<br>";
-	echo "1st floor 0th area: " . busynessIndex('Robarts Library', 1, 0) . "<br>";
+	echo "1st floor 1st area: " . busynessIndex('Robarts Library', 1, 1) . "<br>";
 	echo "2nd floor: " . busynessIndex('Robarts Library', 2) . "<br>";
-	echo "2nd floor 0th area: " . busynessIndex('Robarts Library', 2, 0) . "<br>";
+	echo "2nd floor 1st area: " . busynessIndex('Robarts Library', 2, 1) . "<br>";
 	
 
 ?>
