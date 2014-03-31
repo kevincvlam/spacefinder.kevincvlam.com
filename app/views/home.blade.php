aa13f41d6b85da9ddd810016b99ab482a9fd0645
kevincvlam <i.am.kev@gmail.com>
2014-03-31 16:08:12 -0400

    off by one error?

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -26,7 +26,7 @@ include '/home7/kevincvl/public_html/spacefinder/library/findBusyness.php';
 <?php
 
 $result = busynessTable('Robarts Library');
-//$row = $result->fetch_array();
+$row = $result->fetch_array();
 for ($i = 1; $i <= 14; $i++) {
 
     // Determine Words Here
