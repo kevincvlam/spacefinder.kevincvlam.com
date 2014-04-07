e8ef93c947542cb13941fcea2761c76eea7f1437
kevincvlam <i.am.kev@gmail.com>
2014-04-07 14:33:15 -0400

    labels

--- app/views/floor.blade.php
+++ app/views/floor.blade.php
@@ -16,7 +16,7 @@
         // Create and draw the visualization.
         new google.visualization.LineChart(document.getElementById('visualization')).
             draw(data, {hAxis: {direction: -1},
-                        vAxis.title: 'Active Connections'
+                        vAxis: {title: 'Active Connections'}
                        }
                 );
       }
